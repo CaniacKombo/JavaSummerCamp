497826084f30f9a328be3923edb0e4cc44d003c8
Joseph <josephsorrell0@gmail.com>
2019-06-10 14:58:52 -0400

    Person and Pokemon
    initial upload of pokemon class practice

---
+++ Pokemon/src/PokemonBattle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PokemonBattle {
+	String p1Attack1;
+	String p1Attack2;
+	String p2Attack1;
+	String p2Attack2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner myObj = new Scanner(System.in);
+	}
+
+}
