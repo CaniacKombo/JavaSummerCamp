00e330d883baf715ead0061b5e6a02758ec7c1bc
Joseph <josephsorrell0@gmail.com>
2019-06-12 11:13:14 -0400

    on 8th challenge
    finished 6 of the challenges and made it to the 8th challenge

--- APCS String Assignment/src/APCSString.java
+++ APCS String Assignment/src/APCSString.java
@@ -12,17 +12,22 @@ public class APCSString {
     public static String writeFirstLast(String name) {
    	 String fullName = "";
    	 //TODO: Complete function code
-   	int comma = name.indexOf(',') + 1;
+   	int comma = name.indexOf(',') ;
+   	int space = name.indexOf(' ');
    	System.out.println (name);
    	System.out.println (comma);
-   	String first = name.substring(0 , comma -1);
+ 	System.out.println (space);
+   	String first = name.substring(0 , comma );
    	String last = "";
-   	if(name.charAt(comma + 1) == ' ') {
-   		last = name.substring(comma + 2);
+   
+   	if(name.charAt(comma ) == ',') {
+   		last = name.substring(comma + 2 , name.length() );
    		System.out.println ( last);
    	}
    		
    	System.out.println (first);
+   	fullName = last + ',' + first;
+//   	System.out.println(fullName);
    	 return fullName;
     }
     
@@ -30,23 +35,47 @@ public class APCSString {
     public static String reverse(String str) {
    	 String out = "";
    	 //TODO: Complete function code
-   	 
+   	 for(int i = str.length() -1 ; i >= 0 ; i--) {
+   		char reverse = str.charAt(i);
+   		out = out + reverse;
+   		
+    		 
+   	 }
+   	 System.out.println(out);
    	 return out;
     }
     
     // 3.
     public static boolean isPalindrome(String str) {
    	 boolean out = true;
+   	 String lowerCase = str.toLowerCase();
+   	 lowerCase =lowerCase.replaceAll("\\W+", "");
    	 //TODO: Complete function code
+   	int firstHalfInt = 0;
+   	int secondHalfInt = lowerCase.length()-1;
+   	if ( lowerCase.length() == 1) {
+   	  return out;
+   	}
+   	for (int i = firstHalfInt; i < lowerCase.length()/2; i++) {
+   	  char firstHalf = lowerCase.charAt( i);
+   	  char secondHalf = lowerCase.charAt (secondHalfInt);
+   	  System.out.println ("firstHalf : " + firstHalf + " secondHalf : " + secondHalf);
+   	  if ( firstHalf != secondHalf){
+   	    out = false;
+   	    break;
+   	  }
+   	  secondHalfInt--;
    	 
-   	 return out;
+    }
+   	return out;
     }
     
     // 4.
     public static String removeDashes(String ssn) {
    	 String out = "";
    	 //TODO: Complete function code
-   	 
+   	out = ssn.replaceAll("-", "");
+  
    	 return out;
     }
     
@@ -54,7 +83,18 @@ public class APCSString {
     public static String changeDateFormat(String dateStr) {
    	 String out = "";
    	 //TODO: Complete function code
+   	 int firstSlash = dateStr.indexOf('/');
+   	 String month = dateStr.substring(0 , firstSlash);
+   	 String withoutMonth = dateStr.substring(firstSlash + 1 , dateStr.length());
+   	 int secondSlash = withoutMonth.indexOf('/');
+   	 String day = withoutMonth.substring(0 , secondSlash);
+   	 String year = withoutMonth.substring(secondSlash + 1 , withoutMonth.length());
+   	 System.out.println(day);
+   	 System.out.println(month);
+   	 System.out.println(withoutMonth);
+	 System.out.println(year);
    	 
+	 out = day + '-' + month + '-' + year;
    	 return out;
     }
     
@@ -62,7 +102,8 @@ public class APCSString {
     public static String lastFour(String ccNumber) {
    	 String out = "";
    	 //TODO: Complete function code
-   	 
+   	 String last4 = ccNumber.substring(ccNumber.length() -4, ccNumber.length());
+   	 out = last4;
    	 return out;
     }
     
@@ -70,7 +111,9 @@ public class APCSString {
     public static String lastFive(String ccNumber) {
    	 String out = "";
    	 //TODO: Complete function code
-   	 
+   	 String ccNumber1 = ccNumber.replaceAll("\\W+", "");
+   	 String last5 = ccNumber1.substring(ccNumber1.length() -5, ccNumber1.length());
+   	 out = last5;
    	 return out;
     }
     
@@ -78,7 +121,10 @@ public class APCSString {
     public static String scroll(String str) {
    	 String out = "";
    	 //TODO: Complete function code
-   	 
+   	 char firstLetter = str.charAt(0);
+   	String wordNoLetter = str.substring(1 , str.length());
+   	String finalWord = wordNoLetter + firstLetter;
+   	 out = finalWord;
    	 return out;
     }
     
@@ -115,7 +161,36 @@ public class APCSString {
    	 System.out.println("Testing Number 1");
    	 String name = "Twain, Mark";
    	 System.out.println(writeFirstLast(name));
-
+   	 //2.
+   	 System.out.println("Testing 2");
+   	 String word = "Applesauce";
+   	 System.out.println(reverse(word));
+   	 //3.
+	 System.out.println("Testing 3");
+   	 String pd = "o";
+   	 System.out.println(isPalindrome(pd));
+   	 //4.
+   	 System.out.println("Testing 4");
+   	 String social = "9491-59-9597";
+   	 System.out.println(removeDashes(social));
+   	 //5.
+   	 System.out.println("Testing 5a 5b");
+   	 String dates = "06/12/2019";
+   	 System.out.println(changeDateFormat(dates));
+   	 //6a
+   	 System.out.println("Testing 6a");
+   	 String lastFore = "1234 1234 1234 4321";
+   	 System.out.println(lastFour(lastFore));
+   	 //6b
+   	 System.out.println("Testing 6b");
+   	 String lastFuve = "1234 1234 1234 4321";
+   	 System.out.println(lastFive(lastFuve));
+   	 //7
+   	 System.out.println("Testing 7");
+   	 String rando = "applesauce";
+   	 System.out.println(scroll(rando));
     }
+    
+    
 
 }
